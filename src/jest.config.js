fe79631f716778e21bbd59c528475311390a155b
StevkovskaMegi <StevkovskaMegi@users.noreply.github.com>
2025-01-03 15:09:54 +0000

    deploy: 9454b24fc1fa86e0821c9df9dbd53359bfde9692

--- src/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  transform: {
-    '^.+\\.jsx?$': 'babel-jest',
-  },
-  moduleNameMapper: {
-    '^axios$': require.resolve('axios'),  // To zagotovi, da bo jest uporabljal pravo različico axios
-  },
-};
